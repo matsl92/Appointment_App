5293b626903532b980e8227884281a32bcd4b679
mateo <mateoslb70@gmail.com>
2023-01-25 13:48:58 -0500

    home page includes user info section, carousel and good looking buttons. Links work properly and design responds for mobile and desktop size

--- appointments/static/js/carousel.js
+++ appointments/static/js/carousel.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 12000,
+      delay: 9000,
       disableOnInteraction: false,
     },
     pagination: {
