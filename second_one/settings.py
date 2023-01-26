52e9ce960da20028972b48e21bc568227af9042e
mateo <mateoslb70@gmail.com>
2023-01-26 12:13:15 -0500

    translated home.html and works properly

--- second_one/settings.py
+++ second_one/settings.py
@@ -136,6 +136,10 @@ LANGUAGES = (
     ('es', _('Spanish')), 
 )
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
